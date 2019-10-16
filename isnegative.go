9cd0f9c12e7ca93a1739eb33c8e54109612801ba
anarzhan94 <anarzhan.abilgazy@nu.edu.kz>
2019-10-16 21:31:34 +0600

    negative

---
+++ isnegative.go
@@ -0,0 +1,23 @@
+package piscine
+
+import "github.com/01-edu/z01"
+
+func IsNegative(nb int) {
+
+	var a rune = 'T'
+	var b rune = 'F'
+
+
+	if nb < 0 {
+
+		
+		z01.PrintRune(a)
+		z01.PrintRune(10)
+
+	} else {
+
+		z01.PrintRune(b)
+		z01.PrintRune(10)
+
+	}
+}
