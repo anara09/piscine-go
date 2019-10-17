f806752a8da0e3bc102a852f4f0cee387ee9659e
anarzhan94 <anarzhan.abilgazy@nu.edu.kz>
2019-10-17 14:58:43 +0600

    comb

--- printcomb.go
+++ printcomb.go
@@ -3,9 +3,9 @@ package piscine
 import "github.com/01-edu/z01"
 
 func PrintComb() {
-	for i := 48 ; i < 58 ; i++ {
-		for j := i+1; j < 58 ; j++ {
-			for k := j+1 ; k < 58 ; k++ {
+	for i := 48; i < 58; i++ {
+		for j := i + 1; j < 58; j++ {
+			for k := j + 1; k < 58; k++ {
 				z01.PrintRune(rune(i))
 				z01.PrintRune(rune(j))
 				z01.PrintRune(rune(k))
@@ -17,5 +17,5 @@ func PrintComb() {
 	}
 	{
 		z01.PrintRune(10)
-	}	
+	}
 }
