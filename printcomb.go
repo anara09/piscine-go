3c98c0123472faf46c89d140093ec1858fde1ae1
anarzhan94 <anarzhan.abilgazy@nu.edu.kz>
2019-10-17 15:47:36 +0600

    cc

--- printcomb.go
+++ printcomb.go
@@ -4,13 +4,16 @@ import "github.com/01-edu/z01"
 
 func PrintComb() {
 	for i := '0'; i < '9'; i++ {
-		for j := i + 1; j < 58; j++ {
-			for k := j + 1; k < 58; k++ {
-				z01.PrintRune(i)
-				z01.PrintRune(j)
-				z01.PrintRune(k)
-				if i <= '6' {
-					z01.PrintRune(44)
+		for j := '0' ; j < 58; j++ {
+			for k := '0' ; k < 58; k++ {
+				if i< j && j< k {
+					z01.PrintRune(i)
+					z01.PrintRune(j)
+					z01.PrintRune(k)
+					if i <= '6' {
+						z01.PrintRune(44)
+						z01.PrintRune(' ')
+					}
 				}
 			}
 		}
