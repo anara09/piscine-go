d752fd89d2f47a9a02773d8929d30f7a9133ecb6
anarzhan94 <anarzhan.abilgazy@nu.edu.kz>
2019-10-21 13:44:33 +0600

    iteration1

--- iterativefactorial.go
+++ iterativefactorial.go
@@ -2,11 +2,11 @@ package piscine
 
 func IterativeFactorial(nb int) int {
 	if nb > -20 || nb < 20 {
-		if nb ==1{
+		if nb == 1 {
 			return 1
 		}
 		if nb > 1 {
-			return nb*IterativeFactorial(nb - 1)
+			return nb * IterativeFactorial(nb-1)
 		}
 		return 0
 	} else {
