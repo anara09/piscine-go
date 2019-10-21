4f1699d3bd2891b463bc1aa6e27afefb5433f58a
anarzhan94 <anarzhan.abilgazy@nu.edu.kz>
2019-10-21 16:30:16 +0600

    fibi

--- fibonacci.go
+++ fibonacci.go
@@ -3,10 +3,9 @@ package piscine
 func Fibonacci(index int) int {
 	if index <= 1 {
 		return index
+	} else if index < 0 {
+		return -1
 	} else {
-		if index > 20 {
-			return 0
-		}
 		return Fibonacci(index-1) + Fibonacci(index-2)
 	}
 }
