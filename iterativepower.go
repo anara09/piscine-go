d6e9efc9b3650f17f65465da14d0a9e0e59bafd9
anarzhan94 <anarzhan.abilgazy@nu.edu.kz>
2019-10-21 15:23:28 +0600

    power

--- iterativepower.go
+++ iterativepower.go
@@ -3,10 +3,12 @@ package piscine
 func IterativePower(nb int, power int) int {
 	if nb > 20 || nb < -20 || power < 0 {
 		return 0
-	} else {
-		if power == 0 {
-			return nb * IterativePower(nb, power-1)
-		}
 	}
-	return 0
+
+	if power == 0 {
+		return 1
+	} else {
+		return nb * IterativePower(nb, power-1)
+	}
+
 }
