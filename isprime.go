3de7280179dc587939c9813bb19d20e654d95c4c
anarzhan94 <anarzhan.abilgazy@nu.edu.kz>
2019-10-21 20:54:25 +0600

    isprime

--- isprime.go
+++ isprime.go
@@ -1,11 +1,12 @@
 package piscine
 
 func IsPrime(nb int) bool {
-	if nb > -200000000 && nb < 200000000 {
-		for i := 2; i < nb; i++ {
-			if nb%i == 0 {
-				return false
-			}
+	if nb == 1 {
+		return true
+	}
+	for i := 2; i < nb; i++ {
+		if nb%i == 0 {
+			return false
 		}
 	}
 	return true
