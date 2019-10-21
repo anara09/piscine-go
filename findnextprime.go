0393214bd815710ec1d2ae79b9d0f5991ac0a61d
anarzhan94 <anarzhan.abilgazy@nu.edu.kz>
2019-10-21 22:49:49 +0600

    findprime

---
+++ findnextprime.go
@@ -0,0 +1,22 @@
+package piscine
+
+func IsPrime(x int) bool {
+	if x == 1 || x == 0 || x < 0 {
+		return false
+	}
+	for i := 2; i < x; i++ {
+		if x%i == 0 {
+			return false
+		}
+	}
+	return true
+}
+
+func FindNextPrime(nb int) int {
+	for i := nb; ; i++ {
+		if IsPrime(i) == true {
+			return i
+		}
+	}
+	return 0
+}
