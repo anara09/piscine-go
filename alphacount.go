17d2c58771be0222284821db12c27369e0256b96
anarzhan94 <anarzhan.abilgazy@nu.edu.kz>
2019-10-22 14:46:10 +0600

    alp

--- alphacount.go
+++ alphacount.go
@@ -4,7 +4,7 @@ func AlphaCount(str string) int {
 	counter := 0
 	for i := 'A'; i <= 'z'; i++ {
 		for _, letter := range str {
-			if i > 'a' && i < 'z' || i > 'A' && i < 'Z' {
+			if i >= 'a' && i <= 'z' || i >= 'A' && i <= 'Z' {
 				if i == letter {
 					counter++
 				}
