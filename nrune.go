5bf41d85ac7153c12f01b3b7aebf56c0cf54c477
anarzhan94 <anarzhan.abilgazy@nu.edu.kz>
2019-10-23 15:36:57 +0600

    nrune

--- nrune.go
+++ nrune.go
@@ -1,15 +1,14 @@
-package piscine 
+package piscine
 
 func NRune(s string, n int) rune {
 	len := 0
-	for range s{
+	for range s {
 		len++
-	} 
-	if n > len || n <1 {
+	}
+	if n > len || n < 1 {
 		return '\x00'
 	} else {
 		arrayStr := []rune(s)
 		return arrayStr[n-1]
 	}
 }
-
