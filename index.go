c047e6ff490dcd862f5c5b1d5ab50cff143c1255
anarzhan94 <anarzhan.abilgazy@nu.edu.kz>
2019-10-23 16:25:12 +0600

    index

--- index.go
+++ index.go
@@ -2,8 +2,8 @@ package piscine
 
 func Index(s string, toFind string) int {
 	for i, letter1 := range s {
-		for j, letter2 := range toFind {
-			if letter1 == letter2 && i == j {
+		for _, letter2 := range toFind {
+			if letter1 == letter2 {
 				return i
 			} else {
 				return -1
