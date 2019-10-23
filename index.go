022491d17ebcad3536611ad76da53bd5dcccb9ef
anarzhan94 <anarzhan.abilgazy@nu.edu.kz>
2019-10-23 16:49:46 +0600

    index

--- index.go
+++ index.go
@@ -4,6 +4,9 @@ func Index(s string, toFind string) int {
 	check := false
 	for i, letter1 := range s {
 		for _, letter2 := range toFind {
+			if letter2 == '\x00' {
+				return 0
+			}
 			if letter1 == letter2 {
 				check = true
 			}
