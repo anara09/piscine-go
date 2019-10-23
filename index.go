d194d6be3d1b848f3a45b118e339e619d611388b
anarzhan94 <anarzhan.abilgazy@nu.edu.kz>
2019-10-23 20:02:49 +0600

    index

--- index.go
+++ index.go
@@ -1,21 +1,32 @@
 package piscine
 
-func Index(s string, toFind string) int {
-	check := false
-	for _, letter2 := range toFind {
-		if letter2 == '\x00' {
-			return 0
+func Index(s, t string) int {
+	ln := 0
+	ln2 := 0
+	for _, c := range s {
+		if c == c {
+			ln++
 		}
 	}
-	for i, letter1 := range s {
-		for _, letter2 := range toFind {
-			if letter1 == letter2 {
-				check = true
+
+	for _, c := range t {
+		if c == c {
+			ln2++
+		}
+	}
+	for i := 0; i < ln; i++ {
+		if ln2 != 0 && s[i] == t[0] {
+			ok := true
+			cur_ch := 0
+			for j := 0; j < ln2; j++ {
+				if i+cur_ch >= ln || t[j] != s[i+cur_ch] {
+					ok = false
+					break
+				}
+				cur_ch++
 			}
-			if check == true {
+			if ok == true {
 				return i
-			} else {
-				break
 			}
 		}
 	}
