9922cd15726508e68945565f32ee65d634e0f8cb
anarzhan94 <anarzhan.abilgazy@nu.edu.kz>
2019-10-24 13:15:48 +0600

    com

--- compare.go
+++ compare.go
@@ -1,22 +1,11 @@
 package piscine
 
 func Compare(a, b string) int {
-	m := []rune(a)
-	l := []rune(b)
-	n := 0
-	k := 0
-	for range m {
-		n++
+	if a == b {
+		return 0
+	} else if a > b {
+		return 1
+	} else {
+		return -1
 	}
-	for range l {
-		k++
-	}
-	for i := 0; i <= n-k; i++ {
-		if b == a[i:i+k] && n == k {
-			return 0
-		} else if b == a[i:i+k] && b[0] == a[0] {
-			return 1
-		}
-	}
-	return -1
 }
